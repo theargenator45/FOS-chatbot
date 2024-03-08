469322991d01871da02168cd42780f8fbb9891d0
David Argent <d.argent62@gmail.com>
2024-03-08 13:09:43 +0000

    patch: get semantic search running

--- src/search.py
+++ src/search.py
@@ -5,6 +5,7 @@ from langchain_community.document_loaders import GCSDirectoryLoader
 from langchain_google_vertexai import VertexAIEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
 
@@ -22,53 +23,54 @@ class SemanticSearch():
         self.model_name = model_name
         
         
-    def initialize_llm(): 
+    def initialize_llm(self): 
         return VertexAI(model_name = self.model_name, 
                         max_output_tokens = 256, 
                         temperature = 0.1, top_p = 0.8, 
                            top_k = 40, verbose = True)
     
-    def load_documents_from_storage(): 
-        return GCSDirectoryLoader(project_name= self.project_id, 
-                                  bucket_name = self.bucket_name, 
+    def load_documents_from_storage(self): 
+        loader = GCSDirectoryLoader(project_name= self.project_id, 
+                                  bucket = self.bucket_name, 
                                   prefix = self.prefix)
+        return loader.load()
     
-    def split_and_chunk(chunk_size, chunk_overlap):
+    def split_and_chunk(self, chunk_size, chunk_overlap, documents):
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=CHUNK_SIZE,
                                                        chunk_overlap=CHUNK_OVERLAP)
         docs = text_splitter.split_documents(documents)
         return docs
     
-    def get_embeddings(requests_per_minute):
+    def get_embeddings(self,requests_per_minute):
         return VertexAIEmbeddings(requests_per_minute= REQUESTS_PER_MINUTE)
     
-    def get_retriever(docs, embeddings):
+    def get_retriever(self,docs, embeddings):
         documents_vector_db = Chroma.from_documents(docs, embeddings)
         return documents_vector_db.as_retriever(search_type = 'mmr', search_kwargs = {"k":3})
     
-    def return_chain(llm, retriever): 
+    def return_chain(self,llm, retriever): 
         return RetrievalQA.from_chain_type(llm = llm, 
                                            chain_type = "stuff", 
                                            retriever=retriever, return_source_documents=True)
     
-    def get_semantic_chain():
-        llm = initialize_llm()
+    def get_semantic_chain(self):
+        llm = self.initialize_llm()
         
-        documents = load_documents_from_storage()
+        documents = self.load_documents_from_storage()
         
         # Split and chunk documents
-        docs = split_and_chunk(CHUNK_SIZE, CHUNK_OVERLAP)
+        docs = self.split_and_chunk(CHUNK_SIZE, CHUNK_OVERLAP, documents)
         
         #get embeddings 
-        embeddings = get_embeddings(REQUESTS_PER_MINUTE)
+        embeddings = self.get_embeddings(REQUESTS_PER_MINUTE)
         
         # Vector search retrieval
         
-        retriever = get_retriever(docs, embeddings)
+        retriever = self.get_retriever(docs, embeddings)
         
         # return _chain 
         
-        semantic_chain = return_chain(llm, retriever)
+        semantic_chain = self.return_chain(llm, retriever)
         
         return semantic_chain
         
