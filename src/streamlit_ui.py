8278ad9093e1edc4f8d5c744113a929f49824e46
Adam <adamhedib@Adams-Laptop.local>
2024-03-08 14:17:08 +0000

    latest

--- src/streamlit_ui.py
+++ src/streamlit_ui.py
@@ -1,14 +1,35 @@
 import streamlit as st
 from langchain_community.callbacks import StreamlitCallbackHandler
+from test_build import create_agent
+import re
 
+logo = "lloyds-logo.webp"
+st.set_page_config(page_icon=logo, page_title="FOS Chatbot")
+st.title("FOS Complaints Chatbot")
 
 @st.cache_resource
 def load_chain():
     # chain goes here
-    return None
+    return create_agent()
+
+def extract_url_and_embed(llm_response):
+    doc_lookup_step = [x for x in llm_response["intermediate_steps"] if x[0].tool == "document_lookup"]
+    if doc_lookup_step:
+        url = re.search("https://(.*?).pdf",doc_lookup_step[-1][1])
+        if url:
+            url = url.group(0)
+            html = f"The source document can be viewed [here]({url})"
+            return html
+
 
 def generate_response(chain):
-    return chain.invoke({"input": st.session_state.messages[-1], "chat_history": st.session_state.messages}, {"callbacks": [StreamlitCallbackHandler(st.container())]})
+    response = chain.invoke({"input": st.session_state.messages[-1], "chat_history": st.session_state.messages}, {"callbacks": [StreamlitCallbackHandler(st.container())]})
+    #print(response["intermediate_steps"])
+    url_html = extract_url_and_embed(response)
+    if url_html:
+        return response["output"] + "\n\n" + url_html
+
+    return response["output"]
 
 def main():
     chain = load_chain()
@@ -17,7 +38,7 @@ def main():
         st.session_state.messages = []
     
     for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
+        with st.chat_message(message["role"], avatar=logo if message["role"] == "assistant" else None):
             st.write(message["content"])
 
     if user_message := st.chat_input():
@@ -31,14 +52,14 @@ def main():
 
 
     if st.session_state.messages and st.session_state.messages[-1]["role"] != "assistant":
-        with st.chat_message("assistant"):
+        with st.chat_message("assistant", avatar=logo):
             response = generate_response(chain)
-            st.write(response["output"])
+            st.write(response)
 
         st.session_state.messages.append(
             {
                 "role": "assistant",
-                "content": response["output"]
+                "content": response
             }
         )
 
