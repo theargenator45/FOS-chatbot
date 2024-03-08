cbd0eeb5219fbdf793eed4bef44e93f9804d2db2
David Argent <d.argent62@gmail.com>
2024-03-08 15:48:09 +0000

    HACKATHON 2024: add all the great stuff

---
+++ sem_search_ui.py
@@ -0,0 +1,46 @@
+import streamlit as st
+from langchain_community.callbacks import StreamlitCallbackHandler
+
+
+@st.cache_resource
+def load_chain():
+    # chain goes here
+    return None
+
+def generate_response(chain):
+    return chain.invoke({"input": st.session_state.messages[-1], "chat_history": st.session_state.messages}, {"callbacks": [StreamlitCallbackHandler(st.container())]})
+
+def main():
+    chain = load_chain()
+    
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+    if user_message := st.chat_input():
+        st.chat_message("user").write(user_message)
+        st.session_state.messages.append(
+            {
+                "role": "user",
+                "content": user_message
+            }
+        ) 
+
+
+    if st.session_state.messages and st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            response = generate_response(chain)
+            st.write(response["output"])
+
+        st.session_state.messages.append(
+            {
+                "role": "assistant",
+                "content": response["output"]
+            }
+        )
+
+if __name__ == "__main__":
+    main()
